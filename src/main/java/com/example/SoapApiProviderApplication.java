61cb5d5240404b6f64a38031dda3792058f21e5c
Viet Tran <vtran@Viets-MacBook-Pro.local>
2017-11-10 09:44:13 -0600

    adding files

---
+++ src/main/java/com/example/SoapApiProviderApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapApiProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapApiProviderApplication.class, args);
+	}
+}
